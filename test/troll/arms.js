1423210f14c543bfe240c468bb932a0505de8ebc
kostmo <kostmo@2466be94-2d0f-11df-9942-15a8a56ed3ad>
2010-10-03 09:09:11 +0000

    testing html hosting

---
+++ test/troll/arms.js
@@ -0,0 +1,433 @@
+var canvas;  
+var ctx;
+var timestep = 0.005;
+var trollface;
+var last_click_pos;
+var scale_size;
+var mouse_button_down = false;
+var GRAVITATIONAL_CONSTANT = 9.81;
+var next_spawn_time = new Date();
+var AVERAGE_SPAWN_SECONDS = 5;
+
+var WHIP_SPEED_THRESHOLD = 8;
+
+var MAX_FLYERS = 5;
+var score = 0;
+
+
+var flying_faces = [];
+var speed_field;
+var distance_field;
+var threshold_field;
+var score_field;
+
+
+
+
+var last_recorded_date;
+var iteration_count;
+var ropeSimulations;
+var keys_pressed;
+var started_simulation;
+var which_click_button = 1;
+
+
+var whip_sound_effect = new Audio("whip.ogg");
+
+
+function MaleFace() {
+	this.happy = "http://ragecomic.appspot.com/packs/happy/images/Happy.png";
+	this.rage = "http://ragecomic.appspot.com/packs/rage/images/OriginalRage.png";
+}
+
+function FemaleFace() {
+	this.happy = "http://ragecomic.appspot.com/packs/happy/images/FemaleHappy.png";
+	this.rage = "http://ragecomic.appspot.com/packs/rage/images/FemaleRage.png";
+}
+
+
+function getFaceImage(male, enraged) {
+	var face = male ? new MaleFace() : new FemaleFace();
+	var img = new Image();
+	img.src = enraged ? face.rage : face.happy;
+	return img;
+}
+
+
+function FlyingFace() {
+
+	this.BASE_PERIOD = 8.0;	// Seconds to traverse distance
+
+	this.angle = Math.random()*2*Math.PI;
+	this.progress = 0;	// A fractional distance from the center
+
+	// FIXME
+//	this.progress = 0.5;
+
+	this.period = this.BASE_PERIOD*(Math.random() + 1/2.0);	// +/- 50%
+	this.rotational_frequency = (Math.random() - 0.5)*5;
+	this.male = Math.random() > 0.5;
+	this.enraged = false;
+
+	this.img = getFaceImage(this.male, this.enraged);
+
+	this.setHit = function() {
+		this.enraged = true;
+		this.img = getFaceImage(this.male, this.enraged);
+	}
+
+
+
+	// The scaled distance from canvas center to any corner
+	this.getMaxDistance = function(width, height) {
+		return new Vector2D(width, height).length()/2.0;
+	}
+
+	this.draw = function(ctx, width, height) {
+
+		var max_distance = this.getMaxDistance(width, height);
+
+		ctx.save();
+			ctx.rotate(this.angle);
+			ctx.translate(max_distance*this.progress, 0);
+			ctx.rotate(this.progress * 2*Math.PI * this.rotational_frequency);
+
+			var scaled_w = this.img.width*this.progress;
+			var scaled_h = this.img.height*this.progress;
+
+			var offset_h = -scaled_w/2.0;
+			var offset_v = -scaled_h/2.0;
+
+			try {
+				// Note: Sometimes Firefox produces a weird Javascript error on this line.
+				ctx.drawImage(this.img, offset_h, offset_v, scaled_w, scaled_h);
+			} catch (e) {
+
+			}
+
+		ctx.restore();
+	}
+
+
+	this.simulate = function(dt) {	// dt must be given in seconds
+		this.progress += dt/this.period;
+	}
+
+	this.isFinished = function() {
+		return this.progress > 1;
+	}
+
+
+	this.getSimulationCoordinates = function(width, height) {
+		var max_distance = this.getMaxDistance(width, height);
+		var scaled_distance = max_distance/scale_size;
+		var vector = new Vector2D( Math.cos(this.angle), -Math.sin(this.angle) )
+		vector.multiply(scaled_distance);
+		return vector;
+	}
+
+
+	this.getRadius = function() {
+		return Math.max(this.img.width, this.img.height)/scale_size;
+	}
+}
+
+
+
+
+function KeysPressed() {
+
+	this.first_click = false;
+
+	this.reset = function() {
+		this.left = false;
+		this.up = false;
+		this.right = false;
+		this.down = false;
+	}
+
+	this.reset();
+
+	this.any = function() {
+		return this.left || this.up || this.right || this.down;
+	}
+}
+
+
+
+function sceneInit() {
+
+	keys_pressed = new KeysPressed();
+	started_simulation = false;
+
+	last_recorded_date = new Date();
+	iteration_count = 0;
+
+	ropeSimulations = new Array(2);
+	for (var i=0; i<ropeSimulations.length; i++)
+		ropeSimulations[i] = new RopeSimulation(
+			i,
+			30,				// 3 Particles (Masses)
+			0.05,				// Each Particle Has A Weight Of 50 Grams
+	//		10000.0,			// springConstant In The Rope
+			1000.0,				// springConstant In The Rope
+			0.06,				// Normal Length Of Springs In The Rope
+			0.2,				// Spring Inner Friction Constant
+			new Vector2D(0, -GRAVITATIONAL_CONSTANT),		// Gravitational Acceleration
+			0.02,				// Air Friction Constant
+			100.0,				// Ground Repel Constant
+			0.2,				// Ground Slide Friction Constant
+			2.0,				// Ground Absoption Constant
+//			-1.5);				// Ground height
+			-10);				// Ground height
+
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	trollface = new Image();
+
+
+	speed_field = document.getElementById("speed_field");
+	distance_field = document.getElementById("distance_field");
+	threshold_field = document.getElementById("threshold_field");
+	score_field = document.getElementById("score_field");
+
+
+	// FIXME - Remove
+//	flying_faces.push( new FlyingFace() );
+
+
+//	trollface.src = 'http://i.imgur.com/VO1NP.jpg';
+	trollface.src = 'http://i.imgur.com/r6WI2.png';
+
+	setInterval(mainIteration, 40);
+}
+
+
+
+function mainIteration() {
+
+	var current_date = new Date();
+	var elapsed_millis = current_date.getTime() - last_recorded_date.getTime();
+	last_recorded_date = current_date;
+
+	if (!started_simulation) {
+		if (keys_pressed.any() || keys_pressed.first_click) {
+			started_simulation = true;
+			keys_pressed.reset();
+		}
+	} else {
+		updateSimulation( elapsed_millis, iteration_count, timestep);
+
+
+
+
+		var now = new Date();
+		if (now.getTime() >= next_spawn_time.getTime()) {
+
+			next_spawn_time = new Date( now.getTime() + Math.random()*AVERAGE_SPAWN_SECONDS*1000 );
+
+			// TODO Maybe should use an Exponential random variable with lambda = 1/AVERAGE_SPAWN_SECONDS;
+
+
+			if (flying_faces.length < MAX_FLYERS) {
+				flying_faces.push( new FlyingFace() );
+			}
+
+		}
+
+		iteration_count++;
+	}
+
+
+
+	scale_size = Math.min(canvas.width, canvas.height)/5.0;
+	draw(canvas.width, canvas.height);
+}
+
+
+
+
+
+function transformClickCoords(click_point) {
+
+	var center_point = new Vector2D(canvas.width, canvas.height);
+	center_point.divide(2.0);
+
+	var centered_click_point = click_point.subtractedBy(center_point);
+	centered_click_point.divide(scale_size);
+
+	return centered_click_point;
+}
+
+
+
+function updateSimulation(milliseconds, iteration_count, maxPossible_dt) {
+
+
+	// Get direction from anchor point to click point
+	if (last_click_pos) {
+		var gravity_vector = transformClickCoords(last_click_pos);
+		gravity_vector.y *= -1;
+		gravity_vector.unitize();
+		gravity_vector.multiply(GRAVITATIONAL_CONSTANT);
+
+		ropeSimulations[which_click_button].gravitation = gravity_vector;
+	}
+
+	var ropeConnectionVel = new Vector2D(0, 0);	// Create A Temporary Vector2D
+
+	if (keys_pressed.left)
+		ropeConnectionVel.x -= 3.0;						// Add Velocity In -X Direction
+
+	if (keys_pressed.right)
+		ropeConnectionVel.x += 3.0;						// Add Velocity In +X Direction
+
+	if (keys_pressed.up)
+		ropeConnectionVel.y += 3.0;						// Add Velocity In +Y Direction
+
+	if (keys_pressed.down)
+		ropeConnectionVel.y -= 3.0;						// Add Velocity In -Y Direction
+
+
+	keys_pressed.reset();
+
+
+	for (var i=0; i<ropeSimulations.length; i++)
+		ropeSimulations[i].setRopeConnectionVel(ropeConnectionVel);		// Set The Obtained ropeConnectionVel In The Simulation
+
+	var dt = milliseconds / 1000.0;	// Converts Milliseconds To Seconds
+
+	for (var i=0; i<flying_faces.length; i++) {
+		var face = flying_faces[i];
+		face.simulate(dt);
+	}
+
+	function isBigEnough(element, index, array) {
+	  return !element.isFinished();
+	}
+
+	flying_faces = flying_faces.filter(isBigEnough);
+
+
+
+	// Calculate Number Of Iterations To Be Made At This Update Depending On maxPossible_dt And dt
+  	var numOfIterations = Math.floor((dt / maxPossible_dt) + 1);			
+	if (numOfIterations != 0)		// Avoid Division By Zero
+		dt = dt / numOfIterations;	// dt Should Be Updated According To numOfIterations
+
+	for (var i=0; i<ropeSimulations.length; i++)
+		for (var a = 0; a < numOfIterations; ++a)	// We Need To Iterate Simulations "numOfIterations" Times
+			ropeSimulations[i].operate(dt, iteration_count);
+}
+
+
+
+function drawGravityRing(pointer_pos) {
+
+	var transformed_click_center = transformClickCoords(pointer_pos);
+	transformed_click_center.y *= -1;
+
+	// Point of button click
+	if (false) {
+		ctx.beginPath();
+		ctx.arc(transformed_click_center.x, transformed_click_center.y, 5*ctx.lineWidth, 0, 2*Math.PI, false);
+		ctx.closePath();
+		ctx.stroke();
+	}
+
+
+	var ring_thickness = 20;
+
+	// Gravity indicator ring
+	var gravity_indicator_radius = 2.5;
+	ctx.save();
+		ctx.lineWidth *= ring_thickness;
+
+		if (which_click_button == 0)
+			ctx.strokeStyle = "rgba(255, 0, 0, 0.25)";
+		else
+			ctx.strokeStyle = "rgba(0, 0, 255, 0.25)";
+
+		ctx.beginPath();
+		ctx.arc(0, 0, gravity_indicator_radius, 0, 2*Math.PI, false);
+		ctx.closePath();
+		ctx.stroke();
+	ctx.restore();
+
+
+	ctx.save();
+//		var grav_delta = transformed_click_center.subtractedBy(ropeSimulations[0].getAnchorMass().pos);
+		var grav_delta = transformed_click_center;
+		ctx.rotate(Math.atan2(grav_delta.y, grav_delta.x));
+		ctx.translate(gravity_indicator_radius, 0);
+		var triangle_overscale = 1.3;
+		ctx.scale(triangle_overscale, triangle_overscale);
+
+		var size = ctx.lineWidth * ring_thickness;
+		// Draw triangle
+		ctx.beginPath();
+		ctx.moveTo(-size/2.0, 0);
+		ctx.lineTo(size/2.0, size/2.0);
+		ctx.lineTo(size/2.0, -size/2.0);
+		ctx.closePath();
+
+		ctx.lineJoin = "round";
+		ctx.stroke();
+		ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+		ctx.fill();
+	ctx.restore();
+}
+
+
+
+function drawArms(width, height) {
+	for (var i=0; i<ropeSimulations.length; i++) {
+		if (i == 0) ctx.strokeStyle = "#F00";
+		else ctx.strokeStyle = "#00F";
+
+		ropeSimulations[i].drawToCanvas(ctx);
+	}
+}
+
+function drawFaces(ctx, width, height) {
+	for (var i=0; i<flying_faces.length; i++) {
+		var face = flying_faces[i];
+
+		face.draw(ctx, width, height);
+	}
+}
+
+function draw(width, height) {
+
+	ctx.clearRect(0, 0, width, height)
+	ctx.lineWidth = 4;
+
+	ctx.beginPath();
+	ctx.rect(0, 0, width, height);
+	ctx.closePath();
+	ctx.stroke();
+
+	ctx.save();
+		ctx.translate(width/2.0, height/2.0);
+
+		ctx.drawImage(trollface, -trollface.width/2, -trollface.height);
+
+
+		ctx.save();
+			ctx.scale(scale_size, -scale_size);
+			ctx.lineWidth /= scale_size;
+
+
+			if (last_click_pos)
+				drawGravityRing(last_click_pos);
+
+			drawArms(width, height);
+
+		ctx.restore();
+
+		drawFaces(ctx, width, height);
+
+
+	ctx.restore();
+}
+
---
+++ test/troll/physics.js
@@ -0,0 +1,479 @@
+function getRand() {
+	return Math.random() - 1/2.0;
+}
+
+// ============================================================================
+function Vector2D(x, y) {
+	this.x = x;
+	this.y = y;
+
+	// Operates on the current instance
+	this.add = function(vec) {
+		this.x += vec.x;
+		this.y += vec.y;
+	};
+
+	// Operates on the current instance
+	this.subtract = function(vec) {
+		this.x -= vec.x;
+		this.y -= vec.y;
+	};
+
+
+	// Operates on the current instance
+	this.multiply = function(scalar) {
+		this.x *= scalar;
+		this.y *= scalar;
+	};
+
+
+	// Operates on the current instance
+	this.divide = function(scalar) {
+		this.x /= scalar;
+		this.y /= scalar;
+	};
+
+
+	this.length = function() {
+		return Math.sqrt(this.x*this.x + this.y*this.y);
+	};
+
+	// Operates on the current instance
+	this.unitize = function() {
+		var len = this.length()
+		this.x /= 1.0*len;
+		this.y /= 1.0*len;
+	};
+
+
+	// Returns a new instance
+	this.dividedBy = function(scalar) {
+		return new Vector2D((1.0/scalar)*this.x, (1.0/scalar)*this.y);
+	};
+
+	// Returns a new instance
+	this.multipliedBy = function(scalar) {
+		return new Vector2D(this.x*scalar, this.y*scalar);
+	};
+
+	// Returns a new instance
+	this.subtractedBy = function(vec) {
+		return new Vector2D(this.x - vec.x, this.y - vec.y);
+	};
+
+	// Returns a new instance
+	this.addedBy = function(vec) {
+		return new Vector2D(this.x + vec.x, this.y + vec.y);
+	};
+}
+
+// ============================================================================
+function Mass(m, i, fixed) {
+
+	this.m = m;				// The mass value
+	this.i = i;				// An identifier for debugging
+
+	this.fixed = fixed;
+
+	this.pos = new Vector2D(0, 0);		// Position in space
+	this.vel = new Vector2D(0, 0);		// Velocity
+	this.force = new Vector2D(0, 0);	// Force applied on this mass at an instance
+
+
+
+	// applyForce(Vector3D force) method is used to add external force to the mass. 
+	// an instance in time, several sources of force might affect the mass. The vector sum 
+	// of these forces make up the net force applied to the mass at the instance.
+
+	this.applyForce = function(f) {
+		if (!this.fixed)
+			this.force.add(f);	// The external force is added to the force of the mass
+	};
+
+
+	this.init = function() {
+		this.force.x = 0;
+		this.force.y = 0;
+	};
+
+
+	// simulate(float dt) method calculates the new velocity and new position of the mass according to change in time (dt). Here, a simulation method called
+	// "The Euler Method" is used. The Euler Method is not always accurate, but it is 
+	//  simple. It is suitable for most of physical simulations that we know in common 
+	//  computer and video games.
+
+	this.simulate = function(dt) {
+
+		// XXX Mass must not be zero!
+		var velocity_delta = this.force.multipliedBy(1.0*dt/this.m);
+
+		// Change in velocity is added to the velocity.
+		// The change is proportinal with the acceleration (force / m) and change in time
+		this.vel.add( velocity_delta );
+									
+		// Change in position is added to the position.
+		// Change in position is velocity times the change in time
+		var position_delta = this.vel.multipliedBy(dt);
+		this.pos.add( position_delta );
+	};
+};
+
+
+// ============================================================================
+// An object to represent a spring with inner friction binding two masses. The spring
+// has a normal length (the length that the spring does not exert any force)
+function Spring(mass1, mass2, springConstant, springLength, frictionConstant) {
+
+	this.springConstant = springConstant;		//set the springConstant			
+	this.springLength = springLength;		//The length that the spring does not exert any force
+	this.frictionConstant = frictionConstant;	//A constant to be used for the inner friction of the spring
+
+	this.mass1 = mass1;										//The first mass at one tip of the spring
+	this.mass2 = mass2;										//The second mass at the other tip of the spring
+
+	this.solve = function() {
+
+		//vector between the two masses
+		var springVector = this.mass1.pos.subtractedBy(this.mass2.pos);
+		var r = springVector.length();	//distance between the two masses
+
+		var force = new Vector2D(0, 0);																//force initially has a zero value
+		if (r != 0) {	//to avoid a division by zero check if r is zero
+			var multiplication_factor = (this.springLength - r) * this.springConstant / r;
+			force.add( springVector.multipliedBy( multiplication_factor ) );	//the spring force is added to the force
+		}
+
+		//the friction force is added to the force
+		//with this addition we obtain the net force of the spring
+		var relative_velocity_vector = this.mass2.vel.subtractedBy(this.mass1.vel);
+		force.add( relative_velocity_vector.multipliedBy(this.frictionConstant) );
+
+		this.mass1.applyForce( force );			//force is applied to mass1
+		this.mass2.applyForce( force.multipliedBy(-1) );	//the opposite of force is applied to mass2
+	}
+
+};
+
+
+// ============================================================================
+// RopeSimulation is derived from class Simulation (see Physics1.h). It simulates a rope with 
+//  point-like particles binded with springs. The springs have inner friction and normal length. One tip of 
+//  the rope is stabilized at a point in space called "Vector3D ropeConnectionPos". This point can be
+//  moved externally by a method "void setRopeConnectionVel(Vector3D ropeConnectionVel)". RopeSimulation 
+//  creates air friction and a planer surface (or ground) with a normal in +y direction. RopeSimulation 
+//  implements the force applied by this surface. In the code, the surface is refered as "ground".
+
+function RopeSimulation(
+		id,
+		num_masses,
+		m,
+		springConstant,
+		springLength,
+		springFrictionConstant,
+		gravitation,	// Vector2D
+		airFrictionConstant,
+		groundRepulsionConstant,
+		groundFrictionConstant,
+		groundAbsorptionConstant,
+		groundHeight)
+{
+	// Since I don't know how to do inheritance in JavaScript properly, I will reimplement all of the methods from the Simulation class.
+
+	this.id = id;
+
+	// My additions...
+	this.current_elapsed_time = 0;
+	this.gravitational_change_time = 0;
+
+
+
+	// superclass initializer
+	this.numOfMasses = num_masses;
+	this.masses = new Array(this.numOfMasses);		// masses are held by pointer to pointer. (Here Mass** represents a 1 dimensional array)
+	
+	for (var count = 0; count < this.numOfMasses; ++count)
+		this.masses[count] = new Mass(m, count, count==0);
+
+	this.ropeConnectionPos = new Vector2D(0, 0);		//A point in space that is used to set the position of the first mass in the system (mass with index 0)
+	this.ropeConnectionVel = new Vector2D(0, 0);		//a variable to move the ropeConnectionPos (by this, we can swing the rope)
+
+
+
+
+	this.gravitation = gravitation;					//gravitational acceleration (gravity will be applied to all masses)
+	this.original_gravitation = gravitation.multipliedBy(1);	// a backup
+	
+	this.current_elapsed_time = 0;
+	this.gravitational_change_time = 0;
+
+	this.airFrictionConstant = airFrictionConstant;			//a constant of air friction applied to masses
+	this.groundFrictionConstant = groundFrictionConstant;		//a constant of friction applied to masses by the ground (used for the sliding of rope on the ground)
+	this.groundRepulsionConstant = groundRepulsionConstant;		//a constant to represent how much the ground shall repel the masses
+	this.groundAbsorptionConstant = groundAbsorptionConstant;	//a constant of absorption friction applied to masses by the ground (used for vertical collisions of the rope with the ground)
+	this.groundHeight = groundHeight;				//a value to represent the y position value of the ground (the ground is a planer surface facing +y direction)
+
+
+	// Initializes the rope in a force-neutral position with respect to the springs
+	for (var index = 0; index < this.numOfMasses; ++index) {
+
+		//Set x position of masses[a] with springLength distance to its neighbor
+		if (this.id == 0)
+			this.masses[index].pos.x = index * springLength;
+		else
+			this.masses[index].pos.x = -index * springLength;
+
+		this.masses[index].pos.y = 0;				//Set y position as 0 so that it stand horizontal with respect to the ground
+	}
+
+
+	//Springs binding the masses (there shall be [numOfMasses - 1] of them)
+	this.springs = new Array(this.numOfMasses - 1);			//create [numOfMasses - 1] pointers for springs
+									//([numOfMasses - 1] springs are necessary for numOfMasses)
+
+	for (var index = 0; index < this.numOfMasses - 1; ++index)			//to create each spring, start a loop
+	{
+		//Create the spring with index "a" by the mass with index "a" and another mass with index "a + 1".
+		this.springs[index] = new Spring(
+			this.masses[index], this.masses[index + 1],
+			springConstant, springLength, springFrictionConstant);
+	}
+
+
+
+	// Duplicated superclass implementation
+	this.init = function()								// this method will call the init() method of every mass
+	{
+		for (var count = 0; count < this.numOfMasses; ++count)
+			this.masses[count].init();
+	}
+
+
+	this.getAnchorMass = function(index) {
+		return this.getMass(0);
+	}
+
+	this.getTipMass = function(index) {
+		return this.getMass( this.numOfMasses - 1);
+	}
+
+
+	// Duplicated superclass implementation
+	this.getMass = function(index) {
+		if (index < 0 || index >= this.numOfMasses)
+			return null;
+
+		return this.masses[index];
+	}
+
+	// solve() is overriden because we have forces to be applied
+	this.solve = function() {
+
+		//apply force of all springs
+		for (var index = 0; index < this.numOfMasses - 1; ++index)
+			this.springs[index].solve();
+
+
+		//Start a loop to apply forces which are common for all masses
+		for (var index = 1; index < this.numOfMasses; ++index) {
+
+			var massObject = this.masses[index];	// TODO Use this
+
+			//The gravitational force
+			massObject.applyForce( this.gravitation.multipliedBy( massObject.m ) );
+
+			//The air friction
+			massObject.applyForce( massObject.vel.multipliedBy( -this.airFrictionConstant ) );
+
+
+			if (massObject.pos.y < this.groundHeight)		//Forces from the ground are applied if a mass collides with the ground
+			{
+				var v = massObject.vel.multipliedBy( 1 );	// get the velocity in a temporary Vector2D
+				v.y = 0;					//omit the velocity component in y direction
+
+				//The velocity in y direction is omited because we will apply a friction force to create
+				//a sliding effect. Sliding is parallel to the ground. Velocity in y direction will be used
+				//in the absorption effect.
+				massObject.applyForce( v.multipliedBy( -this.groundFrictionConstant ) );		//ground friction force is applied
+
+				v = massObject.vel.multipliedBy( 1 );		//get the velocity in a temporary Vector2D
+				v.x = 0;						//omit the x component of the velocity
+
+				//above, we obtained a velocity which is vertical to the ground and it will be used in
+				//the absorption force
+
+				if (v.y < 0) //let's absorb energy only when a mass collides towards the ground
+					massObject.applyForce( v.multipliedBy( -this.groundAbsorptionConstant ) ); //the absorption force is applied
+
+				//The ground shall repel a mass like a spring.
+				//By "Vector3D(0, groundRepulsionConstant, 0)" we create a vector in the plane normal direction
+				//with a magnitude of groundRepulsionConstant.
+				//By (groundHeight - masses[a]->pos.y) we repel a mass as much as it crashes into the ground.
+				var force = new Vector2D(0, this.groundRepulsionConstant).multipliedBy(this.groundHeight - massObject.pos.y);
+
+				massObject.applyForce( force );			//The ground repulsion force is applied
+			}
+		}
+	};
+
+
+
+
+	//simulate(float dt) is overriden because we want to simulate
+	//the motion of the ropeConnectionPos
+	this.simulate = function(dt) {
+
+		this.ropeConnectionPos.add( this.ropeConnectionVel.multipliedBy( dt ) );	//iterate the positon of ropeConnectionPos
+
+		if (this.ropeConnectionPos.y < this.groundHeight)			//ropeConnectionPos shall not go under the ground
+		{
+			this.ropeConnectionPos.y = this.groundHeight;
+			this.ropeConnectionVel.y = 0;
+		}
+
+		this.masses[0].pos = this.ropeConnectionPos;				//mass with index "0" shall position at ropeConnectionPos
+		this.masses[0].vel = this.ropeConnectionVel;				//the mass's velocity is set to be equal to ropeConnectionVel
+
+
+
+		// A simple, two-line superclass implementation
+		for (var count = 0; count < this.numOfMasses; ++count)		// We will iterate every mass
+			this.masses[count].simulate(dt);				// Iterate the mass and obtain new position and new velocity
+
+
+
+
+		var whip_tip = this.getTipMass();
+		var tip_speed = whip_tip.vel.length();
+
+
+
+
+
+
+		if (tip_speed > WHIP_SPEED_THRESHOLD) {
+//		if (true) {	// FIXME
+
+			var canvas_width = canvas.width;
+			var canvas_height = canvas.height;
+
+
+
+			// Check to see if the whip has struck any faces.
+			for (var i = 0; i < flying_faces.length; ++i) {
+				var face = flying_faces[i];
+
+				var face_coords = face.getSimulationCoordinates(canvas_width, canvas_height);
+				var pos_delta = whip_tip.pos.subtractedBy(face_coords);
+				var tip_distance = pos_delta.length();
+
+
+				distance_field.value = tip_distance;
+
+
+				var impact_radius = face.getRadius();
+				threshold_field.value = impact_radius;
+
+
+
+				if (tip_distance < impact_radius) {
+					if (!face.enraged) {
+						face.setHit();
+						score++;
+						score_field.innerHTML = score;
+						whip_sound_effect.play();
+					}
+				}
+			}
+		}
+	}
+
+	//the method to set ropeConnectionVel (accepts a Vector2D)
+	this.setRopeConnectionVel = function(ropeConnectionVel) {
+		this.ropeConnectionVel = ropeConnectionVel;
+	}
+
+
+	this.operate = function(dt, main_loop_iteration_number) {
+
+		// FIXME Periodically apply random gravity?
+		this.current_elapsed_time += dt;
+//		printf("Current elapsed time: %0.3f\n", current_elapsed_time);
+
+		if (false) {
+//		if (current_elapsed_time >= gravitational_change_time) {
+			this.gravitational_change_time = this.current_elapsed_time + 0.5;	// Every two seconds, change the gravity.
+
+			var random_vector = new Vector2D(getRand(), getRand());	// Genearate a random direction vector, with values between -1 and 1
+			random_vector.unitize();
+			random_vector.multiply(original_gravitation.length()/8.0);
+
+			this.gravitation = this.random_vector;
+		}
+
+
+		// Full superclass implementation:
+		this.init();				// Step 1: reset forces to zero
+		this.solve();				// Step 2: apply forces
+		this.simulate(dt);
+	}
+
+	// Draw the arms as connected line segments
+	this.drawToCanvas = function(ctx) {
+
+		ctx.beginPath();
+
+		var pos = this.getAnchorMass().pos;
+		ctx.moveTo(pos.x, pos.y);
+
+		for (var index = 1; index < this.numOfMasses; ++index) {
+			var pos = this.getMass(index).pos;
+			ctx.lineTo(pos.x, pos.y);
+		}
+		ctx.lineJoin = "round";
+
+
+		var tip_speed = this.getTipMass().vel.length();
+		if (this.id == 0) {
+			speed_field.value = tip_speed;
+		}
+
+		if (tip_speed > WHIP_SPEED_THRESHOLD) {
+			ctx.save();
+				ctx.lineWidth *= 3;
+				ctx.strokeStyle = "#0F0";
+				ctx.stroke();
+			ctx.restore()
+		}
+
+
+		ctx.stroke();
+	}
+
+
+
+/*
+	// Draw the arms as connected (quadratic) Bezier curves
+	this.drawToCanvas = function(ctx) {
+
+		ctx.beginPath();
+
+		var pos = this.getAnchorMass().pos;
+		ctx.moveTo(pos.x, pos.y);
+
+		var last_pos = pos;
+		var penultimate_pos = last_pos;
+		for (var index = 1; index < this.numOfMasses; ++index) {
+			var pos = this.getMass(index).pos;
+//			ctx.bezierCurveTo(last_pos.x, last_pos.y, pos.x, pos.y, pos.x, pos.y);
+
+			var prev_extension = last_pos.addedBy( last_pos.subtractedBy(penultimate_pos) );
+			ctx.quadraticCurveTo(prev_extension.x, prev_extension.y, pos.x, pos.y);
+
+			penultimate_pos = last_pos;
+			last_pos = pos;
+		}
+		ctx.lineJoin = "round";
+		ctx.stroke();
+	}
+*/
+};
